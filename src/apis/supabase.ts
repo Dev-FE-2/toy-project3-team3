d0eca7b257a007245a1a4e375ed045695b548d1d
yulsanoh <yulsanohdev@gmail.com>
2025-01-07 16:45:46 +0900

    chore: supbase.ts 파일 내 타입 지정 (#2)

--- src/apis/supabase.ts
+++ src/apis/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '@/types/database.types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
@@ -7,4 +8,4 @@ if (!supabaseUrl || !supabaseKey) {
   throw new Error('❌Supabase 환경 변수를 확인해주세요.');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
