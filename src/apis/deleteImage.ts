a13f4bcb7e1fa79853ecaef84995beb83fcd5642
vgotu99 <vgotu99@gmail.com>
2025-01-07 11:16:29 +0900

    feature: 이미지 삭제 시 url을 받아 삭제되도록 변경 (#11)

--- src/apis/deleteImage.ts
+++ src/apis/deleteImage.ts
@@ -1,9 +1,7 @@
 import { supabase } from '@/apis/supabase';
 
-// 📌 수정 필요!
 export const deleteImage = async (imageUrl: string): Promise<void> => {
   const filePath = imageUrl.split('/').slice(-2).join('/');
-  console.log(filePath);
   const { error } = await supabase.storage.from('GRAM').remove([filePath]);
 
   if (error) {
