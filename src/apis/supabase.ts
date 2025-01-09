6f2c10eacefbb20aa975300cf50826846205a789
vgotu99 <vgotu99@gmail.com>
2025-01-09 20:58:08 +0900

    feature: axios를 이용하여 supabase DB의 데이터를 CRUD를 하기위한 설정 (#22)

--- src/apis/supabase.ts
+++ src/apis/supabase.ts
@@ -1,11 +1,21 @@
+import axios from 'axios';
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/types/database.types';
+import type { Database } from '@/types/database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
+if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error('❌Supabase 환경 변수를 확인해주세요.');
 }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabaseDB = axios.create({
+  baseURL: `${SUPABASE_URL}/rest/v1`,
+  headers: {
+    apikey: SUPABASE_KEY,
+    Authorization: `Bearer ${SUPABASE_KEY}`,
+    'Content-Type': 'application/json',
+  },
+});
+
+export const supabaseAuth = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
--- src/contexts/SupabaseProvider.tsx
+++ src/contexts/SupabaseProvider.tsx
@@ -1,17 +1,11 @@
-import React, { createContext, ReactNode, useEffect, useState } from 'react';
-import { supabase } from '../apis/supabase';
-import { Session, User } from '@supabase/supabase-js';
-
-type SupabaseContextType = {
-  session: Session | null;
-  user: User | null;
-};
+import { ReactNode, useEffect, useState } from 'react';
+import type { Session, User } from '@supabase/supabase-js';
+import { supabaseAuth } from '../apis/supabase';
+import { SupabaseContext } from '@/contexts/SupabaseContext';
 
 type SupabaseProviderProps = {
   children: ReactNode;
 };
-
-const SupabaseContext = createContext<SupabaseContextType | null>(null);
 
 export function SupabaseProvider({ children }: SupabaseProviderProps) {
   const [session, setSession] = useState<Session | null>(null);
@@ -19,14 +13,14 @@ export function SupabaseProvider({ children }: SupabaseProviderProps) {
 
   useEffect(() => {
     const getSession = async () => {
-      const { data } = await supabase.auth.getSession();
+      const { data } = await supabaseAuth.auth.getSession();
       setSession(data?.session || null);
       setUser(data?.session?.user || null);
     };
 
     getSession();
 
-    const { data: listener } = supabase.auth.onAuthStateChange(
+    const { data: listener } = supabaseAuth.auth.onAuthStateChange(
       (_event, session) => {
         setSession(session);
         setUser(session?.user || null);
@@ -43,13 +37,4 @@ export function SupabaseProvider({ children }: SupabaseProviderProps) {
       {children}
     </SupabaseContext.Provider>
   );
-}
-
-export function useSupabase() {
-  const context = React.useContext(SupabaseContext);
-  if (!context) {
-    throw new Error(
-      '❌useSupabase를 사용하려면 SupabaseProvider의 내부에 작성해야 합니다.',
-    );
-  }
 }
