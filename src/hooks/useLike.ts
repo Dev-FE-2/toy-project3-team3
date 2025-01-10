6e3668f14aea53d35b7f79689008b67e95c1feb2
vgotu99 <vgotu99@gmail.com>
2025-01-11 07:05:40 +0900

    feature: likes 테이블 커스텀 훅 구현 (#22)

---
+++ src/hooks/useLike.ts
@@ -0,0 +1,69 @@
+import { API_ENDPOINTS, QUERY_KEYS } from '@/constants';
+import {
+  useCreateData,
+  useDeleteDataByTwoId,
+  useFetchDataAll,
+  useFetchDataByOneId,
+  useFetchDataByTwoId,
+  useUpdateDataByTwoId,
+} from '@/hooks/useSupabaseCrud';
+import type { Database } from '@/types';
+
+const { LIKES } = API_ENDPOINTS;
+const queryKey = [QUERY_KEYS.LIKES];
+
+export const useFetchLikes = () =>
+  useFetchDataAll<Database['public']['Tables']['LIKES']['Row']>(
+    queryKey,
+    LIKES.BASE,
+  );
+
+export const useFetchLikeByPlaylistId = (playlistId: string) =>
+  useFetchDataByOneId<Database['public']['Tables']['LIKES']['Row']>(
+    [QUERY_KEYS.LIKES, playlistId],
+    LIKES.BY_PLAYLIST_ID,
+    playlistId,
+  );
+
+export const useFetchLikeByCommentId = (commentId: string) =>
+  useFetchDataByOneId<Database['public']['Tables']['LIKES']['Row']>(
+    [QUERY_KEYS.LIKES, commentId],
+    LIKES.BY_COMMENT_ID,
+    commentId,
+  );
+
+export const useFetchLikeByPlaylistIdAndUserId = (
+  playlistId: string,
+  userId: string,
+) =>
+  useFetchDataByTwoId<Database['public']['Tables']['LIKES']['Row']>(
+    [QUERY_KEYS.LIKE, userId, playlistId],
+    LIKES.BY_PLAYLIST_ID_AND_USER_ID,
+    playlistId,
+    userId,
+  );
+
+export const useFetchLikeByCommentIdAndUserId = (
+  commentId: string,
+  userId: string,
+) =>
+  useFetchDataByTwoId<Database['public']['Tables']['LIKES']['Row']>(
+    [QUERY_KEYS.LIKE, userId, commentId],
+    LIKES.BY_COMMENT_ID_AND_USER_ID,
+    commentId,
+    userId,
+  );
+
+export const useCreateLike = () => useCreateData<'LIKES'>(queryKey, LIKES.BASE);
+
+export const useUpdateLikeByPlaylistIdAndUserId = () =>
+  useUpdateDataByTwoId<'LIKES'>(queryKey, LIKES.BY_PLAYLIST_ID_AND_USER_ID);
+
+export const useUpdateLikeByCommentIdAndUserId = () =>
+  useUpdateDataByTwoId<'LIKES'>(queryKey, LIKES.BY_COMMENT_ID_AND_USER_ID);
+
+export const useDeleteLikeByPlaylistIdAndUserId = () =>
+  useDeleteDataByTwoId(queryKey, LIKES.BY_PLAYLIST_ID_AND_USER_ID);
+
+export const useDeleteLikeByCommentIdAndUserId = () =>
+  useDeleteDataByTwoId(queryKey, LIKES.BY_COMMENT_ID_AND_USER_ID);
