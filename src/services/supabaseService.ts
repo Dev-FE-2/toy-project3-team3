4a65274a208f8ad1d42af7b6a1d57cf4d18b2f74
vgotu99 <vgotu99@gmail.com>
2025-01-11 03:32:01 +0900

    feature: 재사용 가능한 supbase db CRUD 서비스 함수 구현 (#22)

---
+++ src/services/supabaseService.ts
@@ -0,0 +1,100 @@
+import { supabaseRest } from '@/apis/supabase';
+
+type EndpointByOneId = (id: string) => string;
+type EndpointByTwoId = (firstId: string, secondId: string) => string;
+
+export const fetchDataAll = async <T>(endpoint: string): Promise<T[]> => {
+  const response = await supabaseRest.get<T[]>(endpoint);
+
+  return response.data;
+};
+
+export const fetchDatahByOneId = async <T>(
+  endpoint: EndpointByOneId,
+  id: string,
+): Promise<T> => {
+  const response = await supabaseRest.get<T[]>(`${endpoint(id)}`);
+
+  return response.data[0];
+};
+
+export const fetchDatahByTwoId = async <T>(
+  endpoint: EndpointByTwoId,
+  firstId: string,
+  secondId: string,
+): Promise<T> => {
+  const response = await supabaseRest.get<T[]>(
+    `${endpoint(firstId, secondId)}`,
+  );
+
+  return response.data[0];
+};
+
+export const fetchDataLengthByOneId = async <T extends { length: number }>(
+  endpoint: EndpointByOneId,
+  id: string,
+): Promise<number> => {
+  const response = await supabaseRest.get<T[]>(`${endpoint(id)}`);
+
+  return response.data.length;
+};
+
+export const fetchDataLengthByTwoId = async <T extends { length: number }>(
+  endpoint: EndpointByTwoId,
+  firstId: string,
+  secondId: string,
+): Promise<number> => {
+  const response = await supabaseRest.get<T[]>(
+    `${endpoint(firstId, secondId)}`,
+  );
+
+  return response.data.length;
+};
+
+export const createData = async <T>(
+  endpoint: string,
+  payload: T,
+): Promise<T> => {
+  const response = await supabaseRest.post<T>(endpoint, payload);
+
+  return response.data;
+};
+
+export const updateDataByOneId = async <T>(
+  endpoint: EndpointByOneId,
+  id: string,
+  payload: Partial<T>,
+): Promise<T> => {
+  const response = await supabaseRest.patch<T>(`${endpoint(id)}`, payload);
+
+  return response.data;
+};
+
+export const updateDataByTwoId = async <T>(
+  endpoint: EndpointByTwoId,
+  firstId: string,
+  secondId: string,
+  payload: Partial<T>,
+): Promise<T> => {
+  const response = await supabaseRest.patch<T>(
+    `${endpoint(firstId, secondId)}`,
+    payload,
+  );
+
+  return response.data;
+};
+
+export const removeDataByOneId = async (
+  endpoint: EndpointByOneId,
+  id: string,
+): Promise<void> => {
+  await supabaseRest.delete(`${endpoint(id)}`);
+};
+
+export const removeDataByTwoId = async (
+  endpoint: EndpointByTwoId,
+  firstId: string,
+  secondId: string,
+): Promise<void> => {
+  await supabaseRest.delete(`${endpoint(firstId, secondId)}`);
+};
