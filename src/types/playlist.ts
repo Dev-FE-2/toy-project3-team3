841ab65ed4a9f2177fff8de2d18bf4e96f4cfdca
vgotu99 <vgotu99@gmail.com>
2025-01-14 22:21:07 +0900

    feature: videoPlayer 컴포넌트에서 사용할 타입 정의 (#9)

---
+++ src/types/playlist.ts
@@ -0,0 +1,20 @@
+declare global {
+  interface Window {
+    YT: {
+      Player: new (
+        elementId: string,
+        config: {
+          height: string;
+          width: string;
+          videoId: string;
+          playerVars?: {
+            autoplay?: 0 | 1;
+            controls?: 0 | 1;
+            rel?: 0 | 1;
+          };
+        },
+      ) => void;
+    };
+    onYouTubeIframeAPIReady: () => void;
+  }
+}
