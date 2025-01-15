6dbf0e2953f9f2b174943a1cc183d4c215161a40
duwlsssss <rladuwls0814@gmail.com>
2025-01-15 11:25:53 +0900

    fix: database type 수정 (#21)

--- src/hooks/useSubscribe.ts
+++ src/hooks/useSubscribe.ts
@@ -1,44 +1,44 @@
-// import { API_ENDPOINTS, QUERY_KEYS } from '@/constants';
-// import {
-//   useCreateData,
-//   useDeleteDataByTwoId,
-//   useFetchDataAll,
-//   useFetchDataByOneId,
-//   useUpdateDataByTwoId,
-// } from '@/hooks/useSupabaseCrud';
-// import type { Database } from '@/types';
+import { API_ENDPOINTS, QUERY_KEYS } from '@/constants';
+import {
+  useCreateData,
+  useDeleteDataByTwoId,
+  useFetchDataAll,
+  useFetchDataByOneId,
+  useUpdateDataByTwoId,
+} from '@/hooks/useSupabaseCrud';
+import type { Database } from '@/types';
 
-// const { SUBSCRIBES } = API_ENDPOINTS;
-// const subscribesQueryKey = QUERY_KEYS.SUBSCRIBES;
+const { SUBSCRIBES } = API_ENDPOINTS;
+const subscribesQueryKey = QUERY_KEYS.SUBSCRIBES;
 
-// export const useFetchSubscribes = () =>
-//   useFetchDataAll<Database['public']['Tables']['SUBSCRIBES']['Row']>(
-//     [subscribesQueryKey],
-//     SUBSCRIBES.BASE,
-//   );
+export const useFetchSubscribes = () =>
+  useFetchDataAll<Database['public']['Tables']['SUBSCRIBES']['Row']>(
+    [subscribesQueryKey],
+    SUBSCRIBES.BASE,
+  );
 
-// export const useFetchSubscribeByUserId = (userId: string) =>
-//   useFetchDataByOneId<Database['public']['Tables']['SUBSCRIBES']['Row']>(
-//     [subscribesQueryKey, userId],
-//     SUBSCRIBES.BY_USER_ID,
-//     userId,
-//   );
+export const useFetchSubscribeByUserId = (userId: string) =>
+  useFetchDataByOneId<Database['public']['Tables']['SUBSCRIBES']['Row']>(
+    [subscribesQueryKey, userId],
+    SUBSCRIBES.BY_USER_ID,
+    userId,
+  );
 
-// export const useFetchSubscribeByPlaylistId = (playlistId: string) =>
-//   useFetchDataByOneId<Database['public']['Tables']['SUBSCRIBES']['Row']>(
-//     [subscribesQueryKey, playlistId],
-//     SUBSCRIBES.BY_PLAYLIST_ID,
-//     playlistId,
-//   );
+export const useFetchSubscribeByPlaylistId = (playlistId: string) =>
+  useFetchDataByOneId<Database['public']['Tables']['SUBSCRIBES']['Row']>(
+    [subscribesQueryKey, playlistId],
+    SUBSCRIBES.BY_PLAYLIST_ID,
+    playlistId,
+  );
 
-// export const useCreateSubscribe = () =>
-//   useCreateData<'SUBSCRIBES'>([subscribesQueryKey], SUBSCRIBES.BASE);
+export const useCreateSubscribe = () =>
+  useCreateData<'SUBSCRIBES'>([subscribesQueryKey], SUBSCRIBES.BASE);
 
-// export const useUpdateSubscribeByIdAndUserId = () =>
-//   useUpdateDataByTwoId<'SUBSCRIBES'>(
-//     [subscribesQueryKey],
-//     SUBSCRIBES.BY_ID_AND_USER_ID,
-//   );
+export const useUpdateSubscribeByIdAndUserId = () =>
+  useUpdateDataByTwoId<'SUBSCRIBES'>(
+    [subscribesQueryKey],
+    SUBSCRIBES.BY_ID_AND_USER_ID,
+  );
 
-// export const useDeleteSubscribeByIdAndUserId = () =>
-//   useDeleteDataByTwoId([subscribesQueryKey], SUBSCRIBES.BY_ID_AND_USER_ID);
+export const useDeleteSubscribeByIdAndUserId = () =>
+  useDeleteDataByTwoId([subscribesQueryKey], SUBSCRIBES.BY_ID_AND_USER_ID);
