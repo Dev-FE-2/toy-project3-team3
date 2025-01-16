458d30401170dc5b98d04c41336f98848abfe8a5
duwlsssss <rladuwls0814@gmail.com>
2025-01-16 10:54:16 +0900

    fix: 하단 nav 팔로잉 path 수정 (#33)

--- src/components/layout/Nav/Nav.tsx
+++ src/components/layout/Nav/Nav.tsx
@@ -11,8 +11,6 @@ const Nav = memo(() => {
   const { user } = useAuth();
   const navigate = useNavigate();
 
-  // 이 부분 수정했습니다
-  // USER_FOLLOW ->MY_FOLLOWING
   const { HOME, PLAY_LIST_EDIT, SEARCH, SIGN_IN, SIGN_UP, MY_FOLLOWING, USER } =
     ROUTES;
 
