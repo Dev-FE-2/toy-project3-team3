4b75c2cb85cbe3092e40e156e1dcbd57f7935245
JOY <92291790+duwlsssss@users.noreply.github.com>
2025-01-17 15:48:00 +0900

    [Feature] 마이 페이지 메뉴 바 (#49)
    * chore: 오류 코드 (#47)
    
    * chore: 코드 업데이트 (#47)
    
    * feature: user 페이지 menuBar 제작(#48)
    
    * fix: database.types.ts thumbnail_image 타입 수정 (#48)
    
    ---------
    
    Co-authored-by: yulsanoh <yulsanohdev@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export * from './common';
 export { Layout } from './layout/Layout';
 export { default as Home } from './home/Home';
-export { default as User } from './user/User';
+export * from './user';
 export * from './play-list-edit';
 export { default as Auth } from './Auth/Auth';
 export { default as PlaylistProtectedRoute } from './PlaylistProtectedRoute/PlaylistProtectedRoute';
--- src/types/common.ts
+++ src/types/common.ts
@@ -139,6 +139,7 @@ export interface TabsProps {
 export interface TabTriggerProps {
   value: number;
   text: string;
+  onClick: () => void;
 }
 
 export interface TabPanelProps {
