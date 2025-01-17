4268b98bc3eca2a1c0ed61d85d24f959fb356e6a
vgotu99 <vgotu99@gmail.com>
2025-01-17 14:04:08 +0900

    design: 플레이리스트 에딧 페이지 배경 UI 설정 (#9)

--- src/pages/PlayListEditPage/PlayListEditPage.styles.ts
+++ src/pages/PlayListEditPage/PlayListEditPage.styles.ts
@@ -7,11 +7,15 @@ export const PlayListEditPageWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   flex: 1;
-  margin-top: ${({ theme }) => theme.space.sm};
 `;
 
 export const Form = styled.form`
   width: 85%;
+  margin-top: ${({ theme }) => theme.space.sm};
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: ${({ theme }) => theme.space.md};
+  border-radius: ${({ theme }) => theme.borderRadius.md};
+  box-shadow: ${({ theme }) => theme.boxShadow};
 `;
 
 export const { SubmitButton } = FormStyle;
