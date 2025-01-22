95fa5cc99360095555ca3fc16be26ddaab90f7d7
seunggeon <vgotu99@gmail.com>
2025-01-22 23:59:31 +0900

    feature: 새로운 댓글 등록을 위한 인풋 추가 및 실시간 업데이트 (#67) (#69)

--- src/components/playlist/playlist/PlaylistContents/CommentList/CommentList.styles.ts
+++ src/components/playlist/playlist/PlaylistContents/CommentList/CommentList.styles.ts
@@ -1,41 +1,10 @@
 import styled from 'styled-components';
 
-export const CommentWrapper = styled.div`
+export const CommentInputAndButtonContainer = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin: ${({ theme }) => theme.space.xsm} 0;
+  align-items: center;
+  gap: ${({ theme }) => theme.space.sm};
+  margin: ${({ theme }) => theme.space.sm} 0;
   padding: ${({ theme }) => theme.space.sm} 0;
   border-bottom: 1px dashed ${({ theme }) => theme.colors.gray.extraLight};
-`;
-
-export const UserWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: ${({ theme }) => theme.space.sm};
-`;
-
-export const IconWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${({ theme }) => theme.space.sm};
-  min-width: 50px;
-`;
-
-export const IconContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: ${({ theme }) => theme.space.xsm};
-`;
-
-export const Comment = styled.div`
-  padding-left: ${({ theme }) => theme.space.sm};
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  word-break: break-all;
 `;
--- src/components/playlist/playlist/PlaylistContents/CommentList/CommentList.tsx
+++ src/components/playlist/playlist/PlaylistContents/CommentList/CommentList.tsx
@@ -1,20 +1,28 @@
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import CommentItems from '@/components/playlist/common/CommentItems/CommentItems';
 import CommentReply from '@/components/playlist/playlist/PlaylistContents/CommentList/CommentReply/CommentReply';
-import { useFetchCommentByTargetPlaylistId } from '@/hooks';
+import {
+  useAuth,
+  useCreateComment,
+  useFetchCommentByTargetPlaylistId,
+} from '@/hooks';
 import { useState } from 'react';
+import { Icon, Input } from '@/components/common';
+import * as S from './CommentList.styles';
 
 const CommentList = () => {
   const { playlistId } = useParams();
+  const { user } = useAuth();
   const [openInputId, setOpenInputId] = useState<string | null>(null);
+  const [newComment, setNewComment] = useState('');
   const loc = useLocation();
   const nav = useNavigate();
   const searchParams = new URLSearchParams(loc.search);
   const target = searchParams.get('target');
 
-  const { data: commentData } = useFetchCommentByTargetPlaylistId(
-    playlistId || '',
-  );
+  const { data: commentData, refetch: refetchCommentData } =
+    useFetchCommentByTargetPlaylistId(playlistId || '');
+  const { mutate: createComment } = useCreateComment();
 
   if (!commentData) return <div>댓글이 없습니다</div>;
 
@@ -27,10 +35,47 @@ const CommentList = () => {
     }
   };
 
+  const handleCreateComment = () => {
+    if (!newComment.trim()) return;
+    createComment(
+      {
+        target_playlist_id: playlistId,
+        user_id: user?.userId || '',
+        comment: newComment,
+      },
+      {
+        onSuccess: () => {
+          refetchCommentData();
+          setNewComment('');
+        },
+      },
+    );
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleCreateComment();
+    }
+  };
+
   const comments = commentData.filter((comment) => !comment.target_comments_id);
 
   return (
     <>
+      {!target && (
+        <S.CommentInputAndButtonContainer>
+          <Input
+            type="text"
+            id="comment"
+            placeholder="새로운 댓글을 입력해주세요"
+            label="댓글"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <Icon type="send" onClick={handleCreateComment} />
+        </S.CommentInputAndButtonContainer>
+      )}
       {target ? (
         <CommentReply target={target} comments={comments} />
       ) : (
